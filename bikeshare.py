fbaab66f43c176ba36310a51212ea7e805a6dac5
delat22 <babalolalateef18@gmai.com>
2020-04-08 16:33:17 +0100

    Comment cities in data used

--- bikeshare.py
+++ bikeshare.py
@@ -4,6 +4,7 @@ import time
 import pandas as pd
 import numpy as np
 
+#Cities in US bikeshare data
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
